56b5c0dd052698166ccbd0fbc741f2e2a8c14a98
brianhanna <bhanna1693@gmail.com>
2020-06-13 10:09:58 -0400

    Initial commit - with spring dependencies for h2, jpa, rest, web, and okta set up

---
+++ src/main/kotlin/com/bhanna/bugtracker/BugTrackerApplication.kt
@@ -0,0 +1,11 @@
+package com.bhanna.bugtracker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BugTrackerApplication
+
+fun main(args: Array<String>) {
+	runApplication<BugTrackerApplication>(*args)
+}
---
+++ src/test/kotlin/com/bhanna/bugtracker/BugTrackerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bhanna.bugtracker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BugTrackerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
