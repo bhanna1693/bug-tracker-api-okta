af7613dc9624954c907271e1ed29fffda0040b6e
brianhanna <bhanna1693@gmail.com>
2020-06-15 22:41:00 -0400

    master - added comments to SecurityConfiguration.kt

--- src/main/kotlin/com/bhanna/bugtracker/SecurityConfiguration.kt
+++ src/main/kotlin/com/bhanna/bugtracker/SecurityConfiguration.kt
@@ -17,13 +17,17 @@ class SecurityConfiguration : WebSecurityConfigurerAdapter() {
                 .and()
                 .oauth2ResourceServer().jwt()
 
+        //Force HTTPS in production
         http.requiresChannel()
                 .requestMatchers(RequestMatcher { r ->
                     r.getHeader("X-Forwarded-Proto") != null
                 }).requiresSecure()
 
+        //Configure the CSRF Cookie so it can be read by JavaScript
         http.csrf()
                 .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+
+        //Configure a CSP that only allows local scripts
         http.headers()
                 .contentSecurityPolicy("script-src 'self'; report-to /csp-report-endpoint/")
         //@formatter:on
